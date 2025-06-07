c18e46a0e22c70c34029d71d2ccedc10bd43f275
rizkybagus381 <155471085+rizkybagus381@users.noreply.github.com>
2025-06-07 22:39:44 +0700

    Add .next/static/chunks/src_app_menu_page_tsx_81df2a71._.js 1749310783389

---
+++ .next/static/chunks/src_app_menu_page_tsx_81df2a71._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_747ec899._.js"
+],
+    source: "dynamic"
+});
