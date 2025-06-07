cd7b9c7563c81541367ca7eb599fea258c934e16
rizkybagus381 <155471085+rizkybagus381@users.noreply.github.com>
2025-06-07 22:39:35 +0700

    Add .next/static/chunks/src_app_admin_page_tsx_81df2a71._.js 1749310775039

---
+++ .next/static/chunks/src_app_admin_page_tsx_81df2a71._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_da922296._.js",
+  "static/chunks/node_modules_@radix-ui_1f77f390._.js"
+],
+    source: "dynamic"
+});
