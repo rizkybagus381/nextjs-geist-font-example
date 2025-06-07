55917f1d32ec915934c8baf9ff24bba65f02a7f2
rizkybagus381 <155471085+rizkybagus381@users.noreply.github.com>
2025-06-07 22:39:37 +0700

    Add .next/static/chunks/src_app_scan_page_tsx_81df2a71._.js 1749310776671

---
+++ .next/static/chunks/src_app_scan_page_tsx_81df2a71._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_e3e21cd8._.js"
+],
+    source: "dynamic"
+});
