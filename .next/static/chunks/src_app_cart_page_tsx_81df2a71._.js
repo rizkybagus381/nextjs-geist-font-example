7c75de772212d2b73170d674487508140b6615c4
rizkybagus381 <155471085+rizkybagus381@users.noreply.github.com>
2025-06-07 22:39:23 +0700

    Add .next/static/chunks/src_app_cart_page_tsx_81df2a71._.js 1749310762443

---
+++ .next/static/chunks/src_app_cart_page_tsx_81df2a71._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_68685482._.js"
+],
+    source: "dynamic"
+});
